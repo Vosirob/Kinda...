fb5bc9ab83ecfa13458abaeb89165ac7c6c55736
Vosirob <alena_borisova201302@rambler.ru>
2023-12-01 12:27:34 +0300

    NewUpdate

--- secondTry/src/App.jsx
+++ secondTry/src/App.jsx
@@ -11,18 +11,18 @@ function App() {
       
       <p className='bluetext'>Начать новый список</p>
 <input type = "text" placeholder='Что нужно делать'></input> <span className='bluetext'> Enter </span> <br/>
-<input type = "checkbox"  className='margin-top'></input><input type = "checkbox" className='checkbox'></input>1. 1<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>2. 2<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>3. 3<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>4. 4<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>5. 5<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>6. 6<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>7. 7<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>8. 8<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>9. Дело 1<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>10. Дело 2<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>11. Дело 3<br/>
-<input type = "checkbox"></input><input type = "checkbox" className='checkbox'></input>12. Дело 4<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox margin-top'></input>1. 1<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>2. 2<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>3. 3<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>4. 4<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>5. 5<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>6. 6<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>7. 7<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>8. 8<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>9. Дело 1<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>10. Дело 2<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>11. Дело 3<br/>
+<img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>12. Дело 4<br/>
 <p className='bluetext'>Удалить выполненные</p>
 
     </>
