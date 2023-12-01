f0eb7e1b17b6620fc8c37b1e5698c4a59ccc5021
Vosirob <alena_borisova201302@rambler.ru>
2023-12-01 21:18:52 +0300

    the beginning of the second mission

--- secondTry/src/App.jsx
+++ secondTry/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import data from './mockData' 
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [defaultData, setDefaultData] = useState(data);
 
-  return (
+  useEffect(() => {console.log(defaultData)}, [defaultData])
+
+
+  return ( 
     <>
       
-      <p className='bluetext'>Начать новый список</p>
-<input type = "text" placeholder='Что нужно делать'></input> <span className='bluetext'> Enter </span> <br/>
+      <button className='bluetext'>Начать новый список</button>
+<input type = "text" placeholder='Что нужно делать'></input> <button className='bluetext'> Enter </button> <br/>
 <img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox margin-top'></input>1. 1<br/>
 <img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>2. 2<br/>
 <img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>3. 3<br/>
@@ -23,7 +29,7 @@ function App() {
 <img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>10. Дело 2<br/>
 <img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>11. Дело 3<br/>
 <img src = {'imgs/x.png'} className='edge'/><input type = "checkbox" className='checkbox'></input>12. Дело 4<br/>
-<p className='bluetext'>Удалить выполненные</p>
+<button className='bluetext'>Удалить выполненные</button>
 
     </>
   )
---
+++ secondTry/src/mockData.js
@@ -0,0 +1,2 @@
+const data = [{name: "сделать практику", isReady: false}, {name: "Сдать практику", isReady: false}, {name: "сделать практику", isReady: false}];
+export default data;
